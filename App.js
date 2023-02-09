8f9125c340f4d362652512a08c548c1b711b4aab
colespen <spencerlewiscole@gmail.com>
2023-02-09 11:30:53 -0500

    integrate uri to access data: sceme sith ImagePIcker

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
+import { useState } from 'react';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      console.log("result: ", result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
+        <Button theme="primary" label="Choose a photo"
+          onPress={pickImageAsync}
+        />
         <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ### working on developing a mobile app with React Native, Expo
 
- - this is a tutorial found on https://docs.expo.dev/tutorial/
+ - this is a tutorial found on https://docs.expo.dev/tutorial/introduction/
